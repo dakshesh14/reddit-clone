d9ee62bea133455a8960fdc14d12412473c50ed9
Dakshesh Jain <dakshesh.jain14@gmail.com>
2022-12-10 16:06:29 +0530

    refactor: moved all the prefix, pre_prefix and suffix to constants.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -5,37 +5,12 @@ import random
 from google.oauth2 import id_token
 from google.auth.transport import requests
 
+# local
+from .constants import prefixes, suffixes, pre_prefixes
+
 
 def get_uuid():
     return str(uuid.uuid4())[:8]
-
-
-prefixes = [
-    "Fluffy", "Fuzzy", "Sparky", "Kitty", "Puppy", "Bunny", "Cuddles", "Smokey",
-    "Peanut", "Buttercup", "Sugar", "Sweetie", "Kisses", "Bubbles", "Angel", "Cupcake",
-    "Darling", "Love", "Honey", "Baby", "Snuggles", "Cotton", "Puddles", "Cupid", "Pookie",
-    "Lucky", "Giggles", "Dream", "Star", "Lovebug", "Honeybun", "Cupcake", "Sugarplum",
-    "Sweetpea", "Honeydew", "Butterbean", "Peaches", "Kissy", "Tootsie", "Honeybee",
-    "Muffin", "Cupcake", "Sweetiepie", "Snickerdoodle", "Snuggly", "Cuddlebug", "Butterball",
-    "Sugarbear"
-]
-suffixes = [
-    "Tiger", "Lion", "Panda", "Giraffe", "Zebra", "Leopard", "Hippo",
-    "Rhino", "Elephant", "Cheetah", "Wolf", "Fox", "Bear", "Moose", "Deer",
-    "Raccoon", "Squirrel", "Skunk", "Rabbit", "Hedgehog", "Otter", "Badger",
-    "Beaver", "Mink", "Weasel", "Ferret", "Sable", "Marten", "Marmot",
-    "Muskrat", "Porcupine", "Seal", "Walrus", "Whale", "Dolphin", "Shark",
-    "Octopus", "Squid", "Lobster", "Crab", "Eel", "Jellyfish", "Starfish",
-    "Coral", "Apple", "Banana", "Orange", "Pear", "Peach", "Plum", "Grape",
-    "Cherry", "Strawberry", "Raspberry", "Blueberry", "Blackberry", "Lemon",
-    "Lime", "Coconut", "Watermelon", "Cantaloupe", "Honeydew", "Mango", "Papaya",
-    "Kiwi", "Guava", "Pineapple", "Pomegranate",
-]
-pre_prefixes = [
-    "Super", "Ultra", "Mega", "Hyper", "Turbo",
-    "Laser", "Plasma", "Neon", "Quantum", "Giga", "Nano",
-    "Atomic", "Galactic", "Cosmic", "Mystic", "Epic", "Legendary",
-]
 
 
 def get_random_name() -> str:
